5cba8c85466f9cb6af189bd51ab2e67b5ef7aadb
James Lal <james@silklabs.com>
2015-11-05 15:06:46 -0800

    Initial working version

---
+++ index.js
@@ -0,0 +1,23 @@
+const plugins = [ 'babel-plugin-syntax-async-functions',
+  'babel-plugin-syntax-class-constructor-call',
+  'babel-plugin-syntax-class-properties',
+  'babel-plugin-syntax-decorators',
+  'babel-plugin-syntax-do-expressions',
+  'babel-plugin-syntax-object-rest-spread',
+  'babel-plugin-transform-async-to-generator',
+  'babel-plugin-transform-class-properties',
+  'babel-plugin-transform-decorators',
+  'babel-plugin-transform-do-expressions',
+  'babel-plugin-transform-es2015-destructuring',
+  'babel-plugin-transform-es2015-modules-commonjs',
+  'babel-plugin-transform-es2015-parameters',
+  'babel-plugin-transform-es2015-sticky-regex',
+  'babel-plugin-syntax-flow',
+  'babel-plugin-transform-flow-strip-types',
+  'babel-plugin-transform-es2015-unicode-regex',
+  'babel-plugin-transform-object-rest-spread' ];
+
+
+module.exports = {
+  plugins
+}
